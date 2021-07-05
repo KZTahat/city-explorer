e9fd6b13f33d4736c04e0dc154eee6d8257471ec
KZTahat <kztahat96@gmail.com>
2021-07-05 19:29:49 +0300

    weather data renderd on request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityName: "-",
+      lat: 0,
+      lon: 0,
+      showMap: false,
+      description: []
+    };
+  }
+
+  getCityInfo = async (event) => {
+    event.preventDefault();
+
+    await this.setState({
+      cityName: event.target.cityName.value,
+    });
+    let url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.cityName}&format=json`;
+    let cityInfo = await axios.get(url);
+
+    await this.setState({
+      lat: cityInfo.data[0].lat,
+      lon: cityInfo.data[0].lon,
+      showMap: true,
+    });
+
+    let weatherData = await axios.get(
+      `${process.env.REACT_APP_SERVER}/getweatherinfo?city_name=${this.state.cityName}`
+    );
+
+    await this.setState({
+      description: weatherData.data.split(',')
+    })
+  };
+
+  render() {
+    return (
+      <>
+        <h1>City Explorer</h1>
+        <form onSubmit={this.getCityInfo}>
+          <input type="text" placeholder="Inter City Name" name="cityName" />
+          <input type="submit" value="Explore!" />
+        </form>
+        <hr />
+
+        <Card style={{ width: "18rem" }}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>City Name: {this.state.cityName}</ListGroup.Item>
+            <ListGroup.Item>Latitude: {this.state.lat}</ListGroup.Item>
+            <ListGroup.Item>longitude: {this.state.lon}</ListGroup.Item>
+          </ListGroup>
+        </Card>
+        {this.state.showMap && (
+          <img
+            src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.lat},${this.state.lon}&zoom=14`}
+          />
+        )}
+
+        <Card style={{ width: "18rem" }}>
+          <ListGroup variant="flush">
+               {this.state.description.map((element,i) => {
+                return <ListGroup.Item key={i}>{this.state.description[i]}</ListGroup.Item>;
+              })}
+          </ListGroup>
+        </Card>
+      </>
+    );
+  }
 }
 
 export default App;
